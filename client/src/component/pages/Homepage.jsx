161b34b51111a2b7ed52ce81a93b8a572ba626d3
harsh6045 <langaliyaharsh2003@gmail.com>
2024-04-06 19:43:26 +0530

    button css added

--- client/src/component/pages/Homepage.jsx
+++ client/src/component/pages/Homepage.jsx
@@ -3,16 +3,47 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const Homepage = () => {
-  let navigate = useNavigate();
-  return (
+ let navigate = useNavigate();
+
+ return (
     <>
       <Navbar />
-      <button onClick={() => navigate("/sanskrit")}>Sanskrit</button>
-      <button onClick={() => navigate("/english")}>English</button>
-      <button onClick={() => navigate("/gujarati")}>Gujarati</button>
-      <button onClick={() => navigate("/hindi")}>Hindi</button>
+      <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-br from-bg-gray-200 to-bg-gray-500">
+        <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            <button
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center"
+              onClick={() => navigate("/sanskrit")}
+            >
+              <span className="text-7xl mb-1">क</span>
+              Sanskrit
+            </button>
+            <button
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center"
+              onClick={() => navigate("/english")}
+            >
+              <span className="text-7xl mb-1">A</span>
+              English
+            </button>
+            <button
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center"
+              onClick={() => navigate("/gujarati")}
+            >
+              <span className="text-7xl mb-1">ક</span>
+              Gujarati
+            </button>
+            <button
+              className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-4 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105 flex flex-col items-center"
+              onClick={() => navigate("/hindi")}
+            >
+              <span className="text-7xl mb-1">क</span>
+              Hindi
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  );
+ );
 };
 
 export default Homepage;
