d3d29a669543419dbe48642f5fa395987b0acbc6
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-04-06 20:45:23 +0530

    Hero Component Updated!

--- client/src/component/pages/Homepage.jsx
+++ client/src/component/pages/Homepage.jsx
@@ -1,56 +1,49 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import heroimage from "../assets/hero.png";
 
 const Homepage = () => {
- let navigate = useNavigate();
+  let navigate = useNavigate();
 
- return (
+  return (
     <>
       <Navbar />
 
       <div className="bg-white">
-      
+        <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
+              <div>
+                <p className="text-base font-semibold tracking-wider text-blue-600 uppercase">
+                  Connect & learn
+                </p>
+                <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">
+                  Learning WebApp
+                </h1>
+                <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl">
+                  Status Code-404
+                </p>
 
-      <section className="bg-[#FCF8F1] bg-opacity-30 py-10 sm:py-16 lg:py-24">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
-            <div>
-              <p className="text-base font-semibold tracking-wider text-blue-600 uppercase">
-                A Language Learning website.......!
-              </p>
-              <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">
-                Connect & learn from our Bots.
-              </h1>
-              <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl">
-                Grow your vocabular skills.
-              </p>
+                <p className="mt-5 text-gray-600">
+                  Already joined us?{" "}
+                  <a
+                    href="#"
+                    title=""
+                    className="text-black transition-all duration-200 hover:underline"
+                  >
+                    Log in
+                  </a>
+                </p>
+              </div>
 
-              
-
-              <p className="mt-5 text-gray-600">
-                Already joined us?{" "}
-                <a
-                  href="#"
-                  title=""
-                  className="text-black transition-all duration-200 hover:underline"
-                >
-                  Log in
-                </a>
-              </p>
-            </div>
-
-            <div>
-              <img
-                className="w-full"
-                src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/1/hero-img.png"
-                alt=""
-              />
+              <div>
+                <img className="w-full" src={heroimage} alt="" />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
       <div className="flex flex-col justify-center items-center min-h-screen bg-white from-bg-gray-200 to-bg-gray-500">
         <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
@@ -86,7 +79,7 @@ const Homepage = () => {
         </div>
       </div>
     </>
- );
+  );
 };
 
 export default Homepage;
